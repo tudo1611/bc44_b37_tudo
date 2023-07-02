392d6b2e7f93b1fa2067aea102835457a7c5b0b1
Do Trung Tu <tudo1611@gmail.com>
2023-07-03 06:56:41 +0700

    add search tab

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { deleteUser } from "./UserReducer";
+
 function Home() {
   const users = useSelector((state) => state.users);
   const dispatch = useDispatch();
+  const [query, setQuery] = useState("");
 
   const handleDelete = (id) => {
     dispatch(deleteUser({ id: id }));
@@ -15,7 +17,14 @@ function Home() {
       <h2>CRUD APP WITH REDUX</h2>
       <Link to={"/create"} className="btn btn-success my-3 align-item-left">
         Create User
-      </Link>
+      </Link>{" "}
+      <br />
+      <input
+        type="text"
+        className="search mb-3"
+        placeholder="Search by name..."
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <table className="table">
         <thead>
           <tr>
@@ -27,25 +36,27 @@ function Home() {
           </tr>
         </thead>
         <tbody>
-          {users.map((user, index) => (
-            <tr key={index}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.phone}</td>
-              <td>
-                <Link to={`/edit/${user.id}`} className="btn btn-primary">
-                  Edit
-                </Link>
-                <button
-                  onClick={() => handleDelete(user.id)}
-                  className="btn btn-danger ml-3"
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
+          {users
+            .filter((user) => user.name.toLowerCase().includes(query))
+            .map((user, index) => (
+              <tr key={index}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>
+                  <Link to={`/edit/${user.id}`} className="btn btn-primary">
+                    Edit
+                  </Link>
+                  <button
+                    onClick={() => handleDelete(user.id)}
+                    className="btn btn-danger ml-3"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
